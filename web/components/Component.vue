5204f6b8fc4c5c13cd77c685c7f8a7309fee81a4
Thien Nguyen <thienguyen.mark@gmail.com>
2021-06-23 21:26:20 +0700

    feat: create an custom element for tiptap

---
+++ web/components/Component.vue
@@ -0,0 +1,49 @@
+<template>
+  <node-view-wrapper class="vue-component">
+    <span class="label" contenteditable="false">Vue Component</span>
+    <node-view-content class="content" />
+  </node-view-wrapper>
+</template>
+
+<script>
+import { NodeViewWrapper, NodeViewContent, nodeViewProps } from '@tiptap/vue-2'
+
+export default {
+  components: {
+    NodeViewWrapper,
+    NodeViewContent,
+  },
+  props: nodeViewProps,
+}
+</script>
+
+<style lang="scss" scoped>
+.vue-component {
+  background: #faf594;
+  border: 3px solid #0d0d0d;
+  border-radius: 0.5rem;
+  margin: 1rem 0;
+  position: relative;
+}
+
+.label {
+  margin-left: 1rem;
+  background-color: #0d0d0d;
+  font-size: 0.6rem;
+  letter-spacing: 1px;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: #fff;
+  position: absolute;
+  top: 0;
+  padding: 0.25rem 0.75rem;
+  border-radius: 0 0 0.5rem 0.5rem;
+}
+
+.content {
+  margin: 2.5rem 1rem 1rem;
+  padding: 0.5rem;
+  border: 2px dashed #0d0d0d20;
+  border-radius: 0.5rem;
+}
+</style>
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -15,7 +15,7 @@
       </ul>
     </div>
     <div class="content-wrapper flex flex-row w-full">
-      <SideBar />
+      <side-bar />
       <div class="h-screen w-4/5 pt-24 px-10">
         <editor v-model="content" class="h-full overflow-y-auto" />
       </div>
@@ -27,7 +27,15 @@
 export default {
   data() {
     return {
-      content: `<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.A Vue.js wrapper component for tiptap to use <code>v-model</code>.</p>`,
+      content: `<p>
+          This is still the text editor you’re used to, but enriched with node views.
+        </p>
+        <vue-component>
+          <p>This is editable.</p>
+        </vue-component>
+        <p>
+          Did you see that? That’s a Vue component. We are really living in the future.
+      </p>`,
     }
   },
 }
@@ -37,6 +45,7 @@ export default {
 .header ul > li {
   @apply px-8 py-2;
 }
+
 .container {
   @apply max-w-none max-h-screen flex flex-row flex-wrap mx-auto;
 }
